5df95254fdcac0a8bf23be49ae8d1df5e60ec780
jlpbiuma <102737801+jlpbiuma@users.noreply.github.com>
2023-01-31 14:12:30 +0000

    Exercise 2 all test passed

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,37 @@ Route::delete('/ejercicio1', function () {
 Route::patch('/ejercicio1', function () {
     return "PATCH OK";
 });
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    // Como es un post, es necesario recibir la request, y de ahí se selecciona lo necesario, en este caso se retorna todo
+    $product = $request->all();
+    return response()->json($product);
+});
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    // Como es un post, es necesario recibir la request, y de ahí se selecciona lo necesario, en este caso se retorna todo
+    $product = $request->all();
+    return $product['price'] > 0 ? response()->json($product) : response()->json(["message" => "Price can't be less than 0"], 422);
+});
+
+Route::post('/ejercicio2/c', function (Request $request) {
+    $product = $request->all();
+    switch($request->query('discount')) {
+        default:
+            $discount = 0;
+            break;
+        case 'SAVE5':
+            $discount = 0.05;
+            break;
+        case 'SAVE10':
+            $discount = 0.1;
+            break;
+        case 'SAVE15':
+            $discount = 0.15;
+            break;
+    }
+    $price = $product['price'] - ($product['price'] * $discount);
+    $product['price'] = $price;
+    $product['discount'] = $discount * 100;
+    return Response::json($product);
+});
